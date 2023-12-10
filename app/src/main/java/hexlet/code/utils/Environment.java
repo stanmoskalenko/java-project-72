6d713e9a5b4a01dd39f21dfbf19de511c34d1250
stanmoskalenko <st.moskalenko@gmail.com>
2023-12-10 18:39:03 +0300

    init

--- app/src/main/java/hexlet/code/utils/Environment.java
+++ app/src/main/java/hexlet/code/utils/Environment.java
@@ -10,8 +10,7 @@ public class Environment {
     private static final String DEFAULT_JTE_TEMPLATE_PATH = "templates";
     private static final String DEFAULT_APP_PORT = "7070";
 
-    public static final String SECRET_PATH = System.getenv("SECRET_PATH");
-    public static final String JDBC_DATABASE_URL = readSecret("JDBC_DATABASE_URL", H2_URL);
+    public static final String JDBC_DATABASE_URL = System.getenv().getOrDefault("JDBC_DATABASE_URL", H2_URL);
     public static final Path SCHEMA_PATH = Path.of(
             System.getenv().getOrDefault("SCHEMA_PATH", DEFAULT_SCHEMA_PATH)).toAbsolutePath().normalize();
     public static final Integer APP_PORT = Integer.parseInt(
@@ -19,18 +18,4 @@ public class Environment {
 
     public static final String TEMPLATE_PATH = System.getenv()
             .getOrDefault("JTE_TEMPLATES_PATH", DEFAULT_JTE_TEMPLATE_PATH);
-
-
-    public static String readSecret(String secretFileName, String defaultValue) {
-        try (InputStream inputStream = Environment.class.getClassLoader()
-                .getResourceAsStream(SECRET_PATH + secretFileName)) {
-            if (inputStream != null) {
-                return inputStream.toString();
-            } else {
-                return defaultValue;
-            }
-        } catch (IOException e) {
-            return defaultValue;
-        }
-    }
 }
